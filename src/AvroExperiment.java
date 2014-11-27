f7b62eacb30c2a137018f74f416f1ae2123bfa37
bajajabhi1 <bajajabhi1@gmail.com>
2014-11-27 14:26:31 -0500

    basic example

--- src/AvroExperiment.java
+++ src/AvroExperiment.java
@@ -1,4 +1,66 @@
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumReader;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import example.avro.User;
+
 
 public class AvroExperiment {
 
+	public static void main(String args[]) {
+		try {
+			File outFile = new File("src/users.avro");
+			serialize(outFile);
+			deserialize(outFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static void serialize(File file) throws IOException {
+
+		User user1 = new User();
+		user1.setName("Alyssa");
+		user1.setFavoriteNumber(256);
+		// Leave favorite color null
+
+		// Alternate constructor
+		User user2 = new User("Ben", 7, "red");
+
+		// Construct via builder
+		User user3 = User.newBuilder().setName("Charlie").
+				setFavoriteColor("blue").setFavoriteNumber(null).build();
+
+
+		// Serialize user1, user2 and user3 to disk
+		DatumWriter<User> userDatumWriter = new SpecificDatumWriter<User>(User.class);
+		DataFileWriter<User> dataFileWriter = new DataFileWriter<User>(userDatumWriter);
+		dataFileWriter.create(user1.getSchema(),file);
+		dataFileWriter.append(user1);
+		dataFileWriter.append(user2);
+		dataFileWriter.append(user3);
+		dataFileWriter.close();
+
+	}
+
+	public static void deserialize(File file) throws IOException {
+		// Deserialize Users from disk
+		DatumReader<User> userDatumReader = new SpecificDatumReader<User>(User.class);
+		DataFileReader<User> dataFileReader = new DataFileReader<User>(file, userDatumReader);
+		User user = null;
+		while (dataFileReader.hasNext()) {
+			// Reuse user object by passing it to next(). This saves us from
+			// allocating and garbage collecting many objects for files with
+			// many items.
+			user = dataFileReader.next(user);
+			System.out.println(user);
+		}
+	}
 }
+
